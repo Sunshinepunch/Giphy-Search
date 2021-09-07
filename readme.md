9f5a818238d74c85857b1182fe1c8018bc149386
Sunshinepunch <78851729+Sunshinepunch@users.noreply.github.com>
2021-09-07 15:48:39 -0700

    finalized with Anna!

---
+++ readme.md
@@ -0,0 +1,42 @@
+# _{Giphy Searcher - API Week}
+
+#### _{This project utilizes giphy API to search for gifs via keyword and has a randomize button}_
+
+#### By _{Becket Harvey & Anna Clarke}_
+
+## _Github Link_ 'Git' your own
+
+## Technologies Used
+
+* _HTML_
+* _CSS_
+* _VSCode_
+* _Javascript_
+* _jQuery_
+* _Git Bash_
+* _Bootstrap_
+* _Node.js_
+* _Postman_
+* _Please review webpack.config & package.json for a full list of dependencies_
+
+
+## Description
+* _This project will take a keyword from the user and display 3 gifs from giphy.com related to that keyword. It will also display a random gif.
+
+## Setup/Installation Requirements
+* Clone project to copy aforementioned webpack and Json configs
+* If you are a Mac user, replace the _&_ in package.json  with a _;_ "start": "npm run build _&_ webpack-dev-server --open --mode=development",
+* Create a developer account with giphy to obtain an API Key
+* Make a .env file (and add to gitignore) and enter API_KEY = {YOUR API KEY HERE}
+* Install Postman and double check your giphy API returns results - api.giphy.com/v1/gifs/search{SEARCHTERM}?q=&{YOUR API KEY HERE}
+* Run npm install, npm run build, npm run start to ensure all pathing is complete and dependencies are working as intended
+
+
+## Known Bugs
+* _None I can find, but if you do be sure to hit me up and let me know!_
+
+## License
+_{MIT}_ This is open source for anyone to use and I am not responsible for any problems resulting from its use or implementation
+
+## Contact Information
+* Find me on Github under SunshinePunch!
--- src/index.js
+++ src/index.js
@@ -3,27 +3,54 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-$(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    const city = $('#location').val();
-    $('#location').val("");
+$(document).ready(function () {
+  $('#giphySearch').click(function () {
+    const gif = $('#keyword').val();
+    $('#keyword').val("");
+
 
     let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-
-    request.onreadystatechange = function() {
+    const url = `http://api.giphy.com/v1/gifs/search?q=${gif}&api_key=${process.env.API_KEY}`;
+    request.onreadystatechange = function () {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
         getElements(response);
       }
-    }
+    };
 
     request.open("GET", url, true);
     request.send();
 
     function getElements(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+      $('.showGifs').html(`<img src="${response["data"][0]["images"]["original"]["url"]}">`);
+      $('.showGifs').append(`<img src="${response["data"][1]["images"]["original"]["url"]}">`);
+      $('.showGifs').append(`<img src="${response["data"][2]["images"]["original"]["url"]}">`);
+
     }
+
+  });
+
+  $('#giphyRandom').click(function (){
+    const gif2 = $('#keyword2').val();
+    $('#keyword2').val("");
+
+    let request = new XMLHttpRequest();
+    const url2 = `http://api.giphy.com/v1/gifs/random?q=${gif2}&api_key=${process.env.API_KEY}`;
+    request.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+    request.open("GET", url2, true);
+    request.send();
+
+    function getElements(response) {
+      $('.showGifs2').html(`<img src="${response["data"]["images"]["original"]["url"]}">`);
+      $('.showGifs2').html(`<img src="${response["data"]["images"]["original"]["url"]}">`);
+      $('.showGifs2').html(`<img src="${response["data"]["images"]["original"]["url"]}">`);
+    }
+
+    
   });
 });
